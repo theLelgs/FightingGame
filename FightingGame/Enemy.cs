f10ce822f4431b2f9ca86e89cc8519ae64349fb2
Linus Lindström <linus@datasnillet.com>
2025-09-09 14:28:39 +0200

    Added Leveling

--- FightingGame/Enemy.cs
+++ FightingGame/Enemy.cs
@@ -9,4 +9,16 @@ public class Enemy
     public int maxHit;
     public int hitChance;
     public string name;
+    public int Attack(int maxHit, int minHit, int hitChance)
+    {
+        if (Random.Shared.Next(1, 100) <= hitChance)
+    {
+        return Random.Shared.Next(minHit, maxHit+1);
+    }
+    else
+    {
+        return 0;
+    }
+
+    }
 }
--- FightingGame/Program.cs
+++ FightingGame/Program.cs
@@ -1,8 +1,8 @@
 ﻿using FightingGame;
 Enemy e1 = new() {hitChance = 50, minHit = 1, maxHit = 4, HP = 30, name="Bob"};
 Enemy e2 = new() {hitChance = 75, minHit = 2, maxHit = 3, HP = 20, name = "David" };
-Enemy p1 = new() {hitChance = 50, minHit = 1, maxHit = 6, HP = 20, name = Console.ReadLine() };
-
+Enemy e3 = new() {hitChance = 50, minHit = 1, maxHit = 6, HP = 20, name = "John" };
+int playerLevel = 1;
 static void Print(string a, int time)
 {
     for (int i = 0; i < a.Length; i++)
@@ -30,32 +30,40 @@ static int Attack(int minHit, int maxHit, int hitChance)
         return 0;
     }
 }
-int playerHP = 20;
-int enemyHP = 20;
-int playerHitChance = 50;
 int enemyHitChance = 50;
+int playerMinHit = 1 ;
+int enemyMinHit = 1;
+int enemyMaxHit = 6;
 Print("What is your name?", 30);
 string playerName = Console.ReadLine();
-List<string> EnemyNames = ["Bob", "David", "Dumstrut"];
-string enemyName = EnemyNames[Random.Shared.Next(0,3)];
-while (playerHP > 0 && enemyHP > 0)
+while (true)
 {
-    enemyHP -= Attack(1, 6, playerHitChance);
-    playerHP -= Attack(1, 6, enemyHitChance);
-    Print($"{enemyName}'s HP is {enemyHP}",150);
-    Print($"{playerName}'s HP is {playerHP}",150);
+int enemyHP = 20;
+int playerHP = 20+3*(playerLevel-1);
+int playerMaxHit = 6+(playerLevel-1);
+int playerHitChance = 50+5*(playerLevel-1);
+    List<string> EnemyNames = ["Bob", "David", "Dumstrut"];
+    string enemyName = EnemyNames[Random.Shared.Next(0, 3)];
+    while (playerHP > 0 && enemyHP > 0)
+    {
+        enemyHP -= Attack(playerMinHit, playerMaxHit, playerHitChance);
+        playerHP -= Attack(enemyMinHit, enemyMaxHit, enemyHitChance);
+        Print($"{enemyName}'s HP is {enemyHP}", 150);
+        Print($"{playerName}'s HP is {playerHP}", 150);
+        Console.ReadLine();
+    }
+    if (enemyHP <= 0 && playerHP >= 0)
+    {
+        Print($"You, {playerName}, won!", 300);
+        playerLevel += 1;
+    }
+    else if (enemyHP >= 0 && playerHP <= 0)
+    {
+        Print($"The enemy, {enemyName}, won!", 300);
+    }
+    else
+    {
+        Print("It's a draw!", 300);
+    }
     Console.ReadLine();
 }
-if (enemyHP <= 0 && playerHP >= 0)
-{
-    Print($"You, {playerName}, won!", 300);
-}
-else if (enemyHP >= 0 && playerHP <= 0)
-{
-    Print($"The enemy, {enemyName}, won!", 300);
-}
-else
-{
-    Print("It's a draw!", 300);
-}
-Console.ReadLine();
